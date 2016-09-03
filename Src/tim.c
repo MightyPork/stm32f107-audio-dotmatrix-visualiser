b317adce956e1cc2c89f368c763142baeb967493
Ondřej Hruška <ondra@ondrovo.com>
2016-09-03 16:57:34 +0200

    comment

--- Src/tim.c
+++ Src/tim.c
@@ -50,7 +50,7 @@ void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 3600; //1800;
+  htim3.Init.Period = 3600; // MX: changed from 1800
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_OC_Init(&htim3) != HAL_OK)
   {
